f80c959c886d8815b50a48cd0a3c29847c8b7ff3
Olivier <olivier@macbook-air-de-olivier.home>
2020-06-05 15:36:40 +0200

    Amelioration controllers posts

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -12,122 +12,95 @@ const Comment = require('../models/Comment');
 const db = require('../models');
 
 
-/*** findAll posts */
+/***** FIND ALL POSTS *****/
 exports.getUserWall = (req, res, next) => {
-  db.sequelize.sync()
-    .then(() => {
-      console.log('database connected...');
-      sequelize.Post.findAll({
-
-          include: {
-            model: sequelize.User,
-            attributes: ["firstname", "lastname", "admin"]
-          },
-          order: [
-            ['createdAt', 'DESC']
-          ]
-        })
-        .then(posts => {
-          console.log(posts);
-          res.status(200).json(posts);
-        })
-        .catch(error => res.status(400).json({
-          error
-        }));
+  sequelize.Post.findAll({
+      include: {
+        model: sequelize.User,
+        attributes: ["firstname", "lastname", "admin"]
+      },
+      order: [
+        ['createdAt', 'DESC']
+      ]
     })
-    .catch(error => res.status(500).json({
+    .then(posts => {
+      console.log(posts);
+      res.status(200).json(posts);
+    })
+    .catch(error => res.status(400).json({
       error
     }));
 }
 
+/***** NEW POSTS *****/
 exports.newPost = (req, res, next) => {
-  db.sequelize.sync()
-    .then(() => {
-      console.log('database connected...');
-      sequelize.Post.create({
-          UserId: req.body.UserId,
-          title: req.body.title,
-          content: req.body.content,
-          url_image: req.body.url_image
-        })
-        .then(response => res.status(200).json({
-          message: "Post bien crée"
-        }))
-        .catch(error => res.status(400).json({
-          error: "Ce post n'as pas pu etre crée"
-        }));
-    })
-    .catch(error => console.log(error));
+  sequelize.Post.create({
+    UserId: req.body.UserId,
+    title: req.body.title,
+    content: req.body.content,
+    url_image: req.body.url_image
+  })
+  .then(response => res.status(200).json({
+    message: "Post bien crée"
+  }))
+  .catch(error => res.status(400).json({
+    error: "Ce post n'as pas pu etre crée"
+  }));
 }
 
+/***** GET ONE POSTS *****/
 exports.getOnePost = (req, res, next) => {
-  db.sequelize.sync()
-    .then(() => {
-      console.log('database connected...');
-      sequelize.Post.findOne({
-          where: {
-            id: req.body.id
-          },
-          include: {
-            model: sequelize.Comment
-            // attributes: ["firstname", "lastname", "admin"]
-          }
-        })
-        .then(post => {
-          console.log(post);
-          res.status(200).json(post);
-        })
-        .catch(error => res.status(400).json({
-          error
-        }));
-    })
-    .catch(error => res.status(500).json({
-      error
-    }));
+  sequelize.Post.findOne({
+    where: {
+      id: req.body.id
+    },
+    include: {
+      model: sequelize.Comment
+      // attributes: ["firstname", "lastname", "admin"]
+    }
+  })
+  .then(post => {
+    console.log(post);
+    res.status(200).json(post);
+  })
+  .catch(error => res.status(400).json({
+    error
+  }));
 }
 
+
+/***** DELETE ON POST *****/
 exports.deleteOnePost = (req, res, next) => {
-  db.sequelize.sync()
-    .then(() => {
-      console.log('database connected...');
-      sequelize.Post.destroy({
-          where: {
-            id: req.body.id
-          }
-        })
-        .then(post => {
-          console.log(post);
-          res.status(200).json({
-            message: "post bien supprimé"
-          });
-        })
-        .catch(error => res.status(400).json({
-          error
-        }));
-    })
-    .catch(error => res.status(500).json({
-      error
-    }));
+  sequelize.Post.destroy({
+    where: {
+      id: req.body.id
+    }
+  })
+  .then(post => {
+    console.log(post);
+    res.status(200).json({
+      message: "post bien supprimé"
+    });
+  })
+  .catch(error => res.status(400).json({
+    error
+  }));
 }
 
+
+/***** UPDATE ONE POST *****/
 exports.updateOnePost = (req, res, next) => {
-  db.sequelize.sync()
-    .then(post => {
-      sequelize.Post.update({
-          title: req.body.title,
-          content: req.body.content,
-          url_image: req.body.url_image
-        }, {
-          where: {
-            id: req.body.id
-          }
-        })
-        .then(response => res.status(200).json({
-          message: "post bien modifié"
-        }))
-        .catch(error => console.log("ERREUR updateValue"));
-    })
-    .catch(error => res.status(500).json({
-      error
-    }));
+  sequelize.Post.update({
+    title: req.body.title,
+    content: req.body.content,
+    url_image: req.body.url_image
+  }, {
+    where: {
+      id: req.body.id
+    }
+  })
+  .then(response => res.status(200).json({
+    message: "post bien modifié"
+  }))
+  .catch(error => console.log("ERREUR updateValue"));
 }
